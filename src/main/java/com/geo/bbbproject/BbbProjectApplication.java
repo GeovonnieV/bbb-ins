287bec5bf5e7dbef26f72b5c194994b73781599f
unknown <geovonnnievasquez@gmail.com>
2021-09-13 11:48:18 -0500

    geo first comit

---
+++ src/main/java/com/geo/bbbproject/BbbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.geo.bbbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbProjectApplication.class, args);
+	}
+
+}
